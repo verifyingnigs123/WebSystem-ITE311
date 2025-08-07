f0ddfe9b88aaaf55dbf61b90059002473f183533
Benz <benzcarllozada123@gmail.com>
2025-08-07 23:00:10 +0800

    Added Bootstrap template and Home controller

---
+++ app/Views/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My WebSystem</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand mx-3" href="#">My WebSystem</a>
+    </nav>
+
+    <div class="container mt-5">
+        <h1>Welcome to the Homepage</h1>
+        <p>This is your new CodeIgniter 4 template using Bootstrap.</p>
+    </div>
+</body>
+</html>
