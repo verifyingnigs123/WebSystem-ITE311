4279cff9fc3eb5ace470eb08c7415a264f35bfce
verifyingnigs123 <raphjohnvisayas@gmail.com>
2025-10-24 03:27:51 +0800

    Add route for viewing course materials by students

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -58,6 +58,8 @@ $routes->get('/admin/course/(:num)/upload', 'Materials::upload/$1');
 $routes->post('/admin/course/(:num)/upload', 'Materials::upload/$1');
 $routes->get('/materials/delete/(:num)', 'Materials::delete/$1');
 $routes->get('/materials/download/(:num)', 'Materials::download/$1');
+$routes->get('/student/course/(:num)/materials', 'Materials::viewByCourse/$1');
+
 
 // Notification routes
 $routes->get('/notifications', 'Notifications::get');
